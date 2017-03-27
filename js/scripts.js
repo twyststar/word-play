3b1f568c24c49b698022d0007713659843497cac
LaTanya Donaldson and Starlynne Start <starlynne@>
2017-03-27 16:58:46 -0700

    Add js to remove words smaller than 3 characters from return.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
 $("#formOne").submit(function(event) {
   event.preventDefault();
+  var lastWords = [];
   var inputWords = $("input#item1").val();
   var splitWords= inputWords.split(" ");
-  //if ()branch for eliminating small words?
-  alert(splitWords);
+  splitWords.forEach(function(word) {
+    if (word.length > 2){
+      lastWords.push(word);
+    }
+  });
+  alert(lastWords);
 
 });
 });
